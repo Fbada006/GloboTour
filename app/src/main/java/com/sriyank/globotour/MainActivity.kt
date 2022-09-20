759db0d4b7f96768d76bd24165b2aca2ec839332
Fbada006 <ferdinandkamata@yahoo.com>
2022-09-20 11:24:02 +0300

    wire up nav component in code

--- app/src/main/java/com/sriyank/globotour/MainActivity.kt
+++ app/src/main/java/com/sriyank/globotour/MainActivity.kt
@@ -2,14 +2,20 @@ package com.sriyank.globotour
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.appbar.MaterialToolbar
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var toolbar: MaterialToolbar
     private lateinit var navController: NavController
+    private lateinit var navigationView: NavigationView
+    private lateinit var drawerLayout: DrawerLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,9 +23,20 @@ class MainActivity : AppCompatActivity() {
 
         // Initialize Views
         toolbar = findViewById(R.id.activity_main_toolbar)
+        navigationView = findViewById(R.id.nav_view)
+        drawerLayout = findViewById(R.id.drawer_layout)
 
         // Get NavHostFragment and NavController
         val navHostFrag = supportFragmentManager.findFragmentById(R.id.nav_host_frag) as NavHostFragment
         navController = navHostFrag.navController
+
+        // Define AppBarConfiguration: Connect DrawerLayout with Navigation Component
+        val appBarConfiguration = AppBarConfiguration(navController.graph, drawerLayout)
+
+        // Connect Toolbar with NavController
+        toolbar.setupWithNavController(navController, appBarConfiguration)
+
+        // Connect NavigationView with NavController
+        navigationView.setupWithNavController(navController)
     }
 }
